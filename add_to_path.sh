5f5e15faa57b24e89485ae5bcc667854521b8b86
Alex Armstrong <armstro1@uci.edu>
2019-01-16 15:47:31 -0800

    Updated add_to_path to account for source/run and being sourced from different directory

--- add_to_path.sh
+++ add_to_path.sh
@@ -1,1 +1,13 @@
-export PYTHONPATH="$PYTHONPATH:${PWD}"
+#!/bin/bash
+#echo "TESTING :: BASH_SOURCE = ${BASH_SOURCE}"
+if [ $0 != "-bash" ]; then
+    echo "ERROR :: Must source directory"
+else
+    prog_dir="$(cd "$(dirname "$(dirname "$BASH_SOURCE")")"; pwd -P)"
+    export PYTHONPATH="$PYTHONPATH:${prog_dir}"
+    echo "$prog_dir added to PYTHONPATH"
+    echo ">> echo PYTHONPATH"
+    echo "."
+    echo "."
+    echo $PYTHONPATH | tr ":" "\n" | tail -n 3
+fi
