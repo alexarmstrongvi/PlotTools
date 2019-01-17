9b783c3fc625d9e52221723a82f80905dc3eb92c
Alex Armstrong <armstro1@uci.edu>
2019-01-16 16:01:23 -0800

    Fixed some bugs

--- add_to_path.sh
+++ add_to_path.sh
@@ -3,11 +3,12 @@
 if [ $0 != "-bash" ]; then
     echo "ERROR :: Must source directory"
 else
-    prog_dir="$(cd "$(dirname "$(dirname "$BASH_SOURCE")")"; pwd -P)"
+    full_path="$(cd "$(dirname "$BASH_SOURCE")" && pwd -P)/$(basename "$1")"
+    prog_dir="$(cd "$(dirname "$full_path")"; pwd -P)"
     export PYTHONPATH="$PYTHONPATH:${prog_dir}"
     echo "$prog_dir added to PYTHONPATH"
-    echo ">> echo PYTHONPATH"
-    echo "."
-    echo "."
+    echo 
+    echo "See for yourself..."
+    echo "Running: echo \$PYTHONPATH | tr \":\" \"\\n\" | tail -n 3"
     echo $PYTHONPATH | tr ":" "\n" | tail -n 3
 fi
