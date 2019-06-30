8f1c1d42c35fb4fe9a7c7f96570b6761c224c9ad
Alex Armstrong <armstro1@uci.edu>
2019-06-30 04:14:15 -0700

    Removed old add_to_path script

--- bash/add_to_path.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $0 != "-bash" ]; then
-    echo "ERROR :: Must source file"
-else
-    full_path="$(cd "$(dirname "$(dirname "$BASH_SOURCE")")" && pwd -P)"
-    prog_dir="$(cd "$(dirname "$full_path")"; pwd -P)"
-    export PYTHONPATH="$PYTHONPATH:${prog_dir}"
-    echo "${prog_dir} added to PYTHONPATH"
-fi
